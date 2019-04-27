b437016b12923d8d64e95aafa8a78597c879e313
CrisESC93 <salazar.cortes.cristian@outlook.com>
2019-04-27 13:12:48 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+const app = express();
+app.use(bodyParser.json());
+
+require('./models/Personas');
+
+mongoose.connect(keys.MONGO_URL);
+
+require('./routes/personasRoutes')(app);
+
+app.listen(process.env.PORT || 5000);
